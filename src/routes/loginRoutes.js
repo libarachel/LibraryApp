d1dfd8338694115720098ac5babfa2888b4477be
libarachel <62275176+libarachel@users.noreply.github.com>
2020-07-02 09:49:01 +0530

    Update loginRoutes.js

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -1,6 +1,8 @@
 const express=require ('express');
+const signupdata = require('../model/signupdata');
 function router(nav){
 const loginRouter=express.Router();
+
 loginRouter.get('/',function(req,res){
     res.render("login",{
         nav,
@@ -9,6 +11,23 @@ loginRouter.get('/',function(req,res){
 
     });
 });
+loginRouter.post('/check',function(req,res){
+    var email=req.body.email;
+console.log(email);
+
+    var password=req.body.password;
+    console.log(password);
+signupdata.findOne({$and :[{'email':email},{'password':password}]},function(err,user){
+    if(user){
+        console.log("valid");
+        res.redirect('/books');
+    }
+    else{
+        
+        res.send("invalid credentials");
+    }
+})
+})
 return loginRouter;
 }
 module.exports=router;
